de888ca482cb1df4763ebe31fd52a036a0c2fd13
yc <seesea2@gmail.com>
2019-07-22 11:14:10 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,9 +15,9 @@ source mail.sh
 
 source 1-nginx.sh
 source 2-letsencrypt.sh
-source 3-mysql.sh
-source 4-postfixadmin.sh
-source 5-postfix.sh
-source 6-dovecot.sh
+# source 3-mysql.sh
+# source 4-postfixadmin.sh
+# source 5-postfix.sh
+# source 6-dovecot.sh
 
-./10-nodejs.sh
+# ./10-nodejs.sh
