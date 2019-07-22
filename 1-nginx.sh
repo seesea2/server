2b87a39175c060c3e6172754f316be13c5b7b080
yc <sgyc@yandex.com>
2019-07-22 20:43:22 +0800

    i

--- 1-nginx.sh
+++ 1-nginx.sh
@@ -9,94 +9,70 @@ systemctl enable nginx
 ufw allow 'Nginx Full'
 
 touch /etc/nginx/snippets/letsencrypt.conf
-myString=
-' server { \
-    listen 80 default_server; \
-    listen [::]:80 default_server; \
-    server_name _; \
-    include snippets/letsencrypt.conf; \
-    return 301 https://$host$request_uri; \
-  } \
-'
-cat $myString >/etc/nginx/sites-available/default
 
-myString = "
-  server {
-    listen 443 ssl default_server;
-    listen [::]:443 default_server;
-    server_name ${myDomain};
-
-    ssl_certificate           /etc/letsencrypt/live/${myDomain}/fullchain.pem;
-    ssl_certificate_key       /etc/letsencrypt/live/${myDomain}/privkey.pem;
-    ssl_trusted_certificate   /etc/letsencrypt/live/${myDomain}/chain.pem;
-
-    include snippets/letsencrypt.conf;
-    location / {
-      proxy_pass http://localhost:8080;
-    }
-  }
-"
-cat $myString >>/etc/nginx/sites-available/default
-
-myString = "
-  server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name mail.${myDomain};
-
-    ssl_certificate           /etc/letsencrypt/live/${myDomain}/fullchain.pem;
-    ssl_certificate_key       /etc/letsencrypt/live/${myDomain}/privkey.pem;
-    ssl_trusted_certificate   /etc/letsencrypt/live/${myDomain}/chain.pem;
-
-    root /var/www/roundcube;
-    index index.php;
-"
-myString = $myString'
-    location / {
-      try_files $uri $uri/ /index.php;
-    }
-
-    location ~* \.php$ {
-      fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-      if (!-f $document_root$fastcgi_script_name) {return 404;}
-      fastcgi_pass  unix:/run/php/php7.2-fpm.sock;
-      fastcgi_index index.php;
-      include fastcgi_params;
-      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    }
-  }
-'
-cat $myString >>/etc/nginx/sites-available/default
-
-myString = "
-  server {
-    listen 443 ssl http2;
-    listen [::]:443 ssl http2;
-    server_name pfa.${myDomain};
-
-    ssl_certificate           /etc/letsencrypt/live/${myDomain}/fullchain.pem;
-    ssl_certificate_key       /etc/letsencrypt/live/${myDomain}/privkey.pem;
-    ssl_trusted_certificate   /etc/letsencrypt/live/${myDomain}/chain.pem;
-
-    root /var/www/postfixadmin;
-    index index.php;
-"
-myString = $myString'
-    location / {
-      try_files $uri $uri/ /index.php;
-    }
-
-    location ~* \.php$ {
-      fastcgi_split_path_info ^(.+?\.php)(/.*)$;
-      if (!-f $document_root$fastcgi_script_name) {return 404;}
-      fastcgi_pass  unix:/run/php/php7.2-fpm.sock;
-      fastcgi_index index.php;
-      include fastcgi_params;
-      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-    }
-  }
-'
-cat $myString >>/etc/nginx/sites-available/default
+{
+  echo "  server { "
+  echo "    listen 80 default_server;"
+  echo "    listen [::]:80 default_server;"
+  echo "    server_name _;"
+  echo "    include snippets/letsencrypt.conf;"
+  echo '    return 301 https://$host$request_uri;'
+  echo "  } "
+  echo "  server { "
+  echo "    listen 443 ssl default_server;"
+  echo "    listen [::]:443 default_server;"
+  echo "    server_name ${myDomain};"
+  echo "    ssl_certificate         /etc/letsencrypt/live/${myDomain}/fullchain.pem;"
+  echo "    ssl_certificate_key     /etc/letsencrypt/live/${myDomain}/privkey.pem;"
+  echo "    ssl_trusted_certificate /etc/letsencrypt/live/${myDomain}/chain.pem;"
+  echo "    include snippets/letsencrypt.conf;"
+  echo "    location / {"
+  echo "      proxy_pass http://localhost:8080;"
+  echo "    }"
+  echo "  }"
+  echo "  server {"
+  echo "    listen 443 ssl http2;"
+  echo "    listen [::]:443 ssl http2;"
+  echo "    server_name mail.${myDomain};"
+  echo "    ssl_certificate           /etc/letsencrypt/live/${myDomain}/fullchain.pem;"
+  echo "    ssl_certificate_key       /etc/letsencrypt/live/${myDomain}/privkey.pem;"
+  echo "    ssl_trusted_certificate   /etc/letsencrypt/live/${myDomain}/chain.pem;"
+  echo "    root /var/www/roundcube;"
+  echo "    index index.php;"
+  echo "    location / {"
+  echo '      try_files $uri $uri/ /index.php;'
+  echo "    }"
+  echo '    location ~* \.php$ {'
+  echo '      fastcgi_split_path_info ^(.+?\.php)(/.*)$;'
+  echo '      if (!-f $document_root$fastcgi_script_name) {return 404;}'
+  echo "      fastcgi_pass  unix:/run/php/php7.2-fpm.sock;"
+  echo "      fastcgi_index index.php;"
+  echo "      include fastcgi_params;"
+  echo '      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;'
+  echo "    }"
+  echo "  }"
+  echo "  server {"
+  echo "    listen 443 ssl http2;"
+  echo "    listen [::]:443 ssl http2;"
+  echo "    server_name pfa.${myDomain};"
+  echo "    ssl_certificate           /etc/letsencrypt/live/${myDomain}/fullchain.pem;"
+  echo "    ssl_certificate_key       /etc/letsencrypt/live/${myDomain}/privkey.pem;"
+  echo "    ssl_trusted_certificate   /etc/letsencrypt/live/${myDomain}/chain.pem;"
+  echo "    root /var/www/postfixadmin;"
+  echo "    index index.php;"
+  echo "    location / {"
+  echo '      try_files $uri $uri/ /index.php;'
+  echo "    }"
+  echo '    location ~* \.php$ {'
+  echo '      fastcgi_split_path_info ^(.+?\.php)(/.*)$;'
+  echo '      if (!-f $document_root$fastcgi_script_name) {return 404;}'
+  echo '      fastcgi_pass  unix:/run/php/php7.2-fpm.sock;'
+  echo "      fastcgi_index index.php;"
+  echo "      include fastcgi_params;"
+  echo '      fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;'
+  echo "    }"
+  echo "  }"
+} >/etc/nginx/sites-available/default
 
 # disable SSL older than TLS1.2
 sed -i 's/TLSv1 //' /etc/nginx/nginx.conf
