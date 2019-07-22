06d8f3bf11595cb7558c04cea5bf9363f9830860
yc <sgyc@yandex.com>
2019-07-22 22:15:48 +0800

    n

--- 4-postfixadmin.sh
+++ 4-postfixadmin.sh
@@ -5,18 +5,18 @@ source config.conf
 wget https://sourceforge.net/projects/postfixadmin/files/latest/download
 tar xvf postfixadmin-*.tar.gz postfixadmin
 
-sudo install postfixadmin/ /var/www/postfixadmin
+install postfixadmin/ /var/www/postfixadmin
 
-sudo cat >/ar/www/postfixadmin/config.local.php <<EOF
-<?php
-$CONF['database_type'] = 'mysqli';
-$CONF['database_user'] = ${mysqlUser};
-$CONF['database_password'] = ${mysqlPass};
-$CONF['database_name'] = ${mysqlDb};
+cat >/ar/www/postfixadmin/config.local.php <<EOF
+  <?php
+  $CONF['database_type'] = 'mysqli';
+  $CONF['database_user'] = ${myDbUser};
+  $CONF['database_password'] = ${myDbPass};
+  $CONF['database_name'] = ${myDb};
 
-$CONF['configured'] = true;
-?>
+  $CONF['configured'] = true;
+  ?>
 EOF
 
-sudo mkdir /var/www/postfixadmin/templates_c && chmod 755 -R /var/www/postfixadmin/templates_c
-sudo chown -R www-data: /var/www/postfixadmin
+mkdir /var/www/postfixadmin/templates_c && chmod 755 -R /var/www/postfixadmin/templates_c
+chown -R www-data: /var/www/postfixadmin
--- setup.sh
+++ setup.sh
@@ -9,14 +9,14 @@ export DEBIAN_FRONTEND=noninteractive
 chmod +x ./*.sh >/dev/null
 
 # upgrade os to latest
-./0-os.sh
+# ./0-os.sh
 
-./1-nginx.sh
-./2-letsencrypt.sh
+# ./1-nginx.sh
+# ./2-letsencrypt.sh
 
-./3-mysql.sh
+# ./3-mysql.sh
 
-# source 4-postfixadmin.sh
+./4-postfixadmin.sh
 # source 5-postfix.sh
 # source 6-dovecot.sh
 
