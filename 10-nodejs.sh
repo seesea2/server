598beba96cdb118ca9c5ecefb0f1573d55d30018
yc <seesea2@gmail.com>
2019-07-23 09:25:41 +0800

    Update 10-nodejs.sh

--- 10-nodejs.sh
+++ 10-nodejs.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
 
 curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
-sudo apt update
-sudo apt -y install nodejs
+sudo -s<<EOF 
+apt-get update
+apt-get -y install nodejs
 
-sudo npm i -g typescript
-sudo npm i -g npm
-sudo npm i -g pm2
+npm i -g typescript
+npm i -g npm
+npm i -g pm2
+EOF
 
-sudo chown -R yc: ~/
+su -c "chown -R yc: ~/"
 
 pm2 startup
 pm2 save
+
+wget https://github.com/seesea2/insg.git
+
+pm2 "insg/dist/server.js"
