724866b6ab271ea5edeec30122738d3c68bd4da6
yc <sgyc@yandex.com>
2019-07-31 23:34:45 +0800

    i

--- 4-mysql.sh
+++ 4-mysql.sh
@@ -6,10 +6,14 @@ echo 'File: '$(basename "$0")
 source global.conf
 
 echo
-echo 'install mysql-server, php-mysql'
+echo '====================== install mysql-server, php-mysql ======================'
 apt-get -y install mysql-server php-mysql
 
+echo
+echo '====================== config mysql database ======================'
 sqlCmd=(
+    "use mysql;"
+    "update user set authentication_string=PASSWORD(\'$myDbPass\') where user='root';"
     "DROP DATABASE IF EXISTS ${myDb};"
     "CREATE DATABASE ${myDb};"
     "GRANT ALL PRIVILEGES ON ${myDb}.* TO '${myDbUser}'@'localhost' IDENTIFIED BY '${myDbPass}';"
